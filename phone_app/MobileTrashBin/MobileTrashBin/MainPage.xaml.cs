2318db86afd0a937a66fe4707fa732da6f969355
Yahia-Sultan <sul,yahia@gmail.com>
2023-09-28 10:08:36 -0400

    Establised and detecting connection with raspberry pi

--- phone_app/MobileTrashBin/MobileTrashBin/MainPage.xaml.cs
+++ phone_app/MobileTrashBin/MobileTrashBin/MainPage.xaml.cs
@@ -1,25 +1,51 @@
 ﻿using InTheHand.Net.Sockets;
 using InTheHand.Net.Bluetooth;
+using InTheHand.Net;
 
 namespace MobileTrashBin;
 
 public partial class MainPage : ContentPage
 {
 
+	BluetoothClient client = new BluetoothClient();
+	BluetoothDeviceInfo rasp = null;
+	// Bluetooth Address: DCA6322171C1 (Byte array is inveresed "C1", "71", "21", "32", "A6", "DC")
+	static byte[] addr = {193, 113, 33, 50, 166, 220};
+    BluetoothAddress raspAdr = new BluetoothAddress(addr);
+
+	private static byte[] StringToByteArray(string str)
+	{
+		return Enumerable.Range(0, str.Length)
+			.Where(x => x % 2 == 0).Select(x => Convert.ToByte(str.Substring(x, 2), 16)).ToArray();
+	}
+
     public MainPage()
 	{
 		InitializeComponent();
 		RequestAccess();
 
-		BluetoothClient client = new BluetoothClient();
 		BluetoothDeviceInfo[] devices = client.PairedDevices.ToArray();
 
 		foreach (BluetoothDeviceInfo device in devices)
 		{
-			freeSp2.Text += device.DeviceName + "\n";
+			System.Diagnostics.Debug.WriteLine("Target : " + device.DeviceAddress);
+			System.Diagnostics.Debug.WriteLine("Comp : " + raspAdr);
+			if (device.DeviceAddress.Equals(raspAdr))
+			{
+				rasp = device;
+				break;
+			}
+        }
+		if (rasp != null)
+        {
+            freeSp.Text = rasp.ClassOfDevice.Value + " " + rasp.DeviceName + " " + rasp.DeviceAddress;
+		}
+		else
+		{
+			freeSp.Text = "Could not find Device";
 		}
 
-		
+
 
 
     }
